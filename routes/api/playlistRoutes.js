a57d79fca7c901cdd77a7f3a713f06f95393ef7d
InfinitiaX <contact@infinitiax.com>
2025-08-21 16:02:19 +0200

    New35 commit

--- routes/api/playlistRoutes.js
+++ routes/api/playlistRoutes.js
@@ -1,4 +1,4 @@
-// routes/api/userplaylists.js
+// routes/api/playlistRoutes.js
 const express = require('express');
 const router = express.Router();
 const playlistController = require('../../controllers/userPlaylistController');
@@ -16,9 +16,11 @@ router.get('/:id', playlistController.getPlaylistById);
 router.put('/:id', protect, playlistController.updatePlaylist);
 router.delete('/:id', protect, playlistController.deletePlaylist);
 router.post('/:id/favorite', protect, playlistController.toggleFavorite);
+router.post('/:id/share', protect, playlistController.sharePlaylist);
 
 // Routes pour les vidéos d'une playlist
 router.post('/:id/videos', protect, playlistController.addVideoToPlaylist);
 router.delete('/:id/videos/:videoId', protect, playlistController.removeVideoFromPlaylist);
+router.put('/:id/reorder', protect, playlistController.reorderPlaylist);
 
 module.exports = router;
